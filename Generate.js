23c1ad3e3adac703cf4017a7a3ccda4c7080ee2b
sparsh0008 <54952775+sparsh0008@users.noreply.github.com>
2021-08-25 13:32:58 +0530

    Almost Complete

--- Generate.js
+++ Generate.js
@@ -3,14 +3,12 @@ var outputDiv    = document.querySelector("#Results");
 
 
 function generateOTP() {
-    console.log("hello");
     var digits = '0123456789';
     let OTP = '';
     for (let i = 0; i < 4; i++ ) {
         OTP += digits[Math.floor(Math.random() * 10)];
     }
     console.log(OTP);
-    outputDiv.textContent = "hello";
 };
 
 btnTranslate.addEventListener("click", generateOTP);
