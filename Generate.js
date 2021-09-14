2cece2ce05438edf4d5ba9b0937e0a6416074c0c
sparsh0008 <54952775+sparsh0008@users.noreply.github.com>
2021-09-14 21:33:39 +0530

    done

--- Generate.js
+++ Generate.js
@@ -2,10 +2,12 @@ var btnTranslate = document.querySelector("#btn-generate");
 var outputDiv    = document.querySelector("#Results");
 
 
-function generateOTP() {
+function generateOTP() 
+{
     var digits = '0123456789';
     let OTP = '';
-    for (let i = 0; i < 4; i++ ) {
+    for (let i = 0; i < 4; i++ ) 
+    {
         OTP += digits[Math.floor(Math.random() * 10)];
     }
     //console.log(OTP);
